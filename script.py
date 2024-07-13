ae3a31d0a2f0e3bb6e999be121d937db87bf7f41
oceanlakra <oceanlakra14@gmail.com>
2024-07-13 20:30:03 +0530

    implemented relative mouse moment(touchpad)

--- script.py
+++ script.py
@@ -3,6 +3,9 @@ import mediapipe as mp
 import pyautogui
 import time
 
+#setting scaling factor for calibrating sensitivity
+scaling_factor = 1.2
+
 # Initialize MediaPipe Hand model
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
@@ -14,6 +17,13 @@ cap = cv2.VideoCapture(2)
 
 # Screen size
 screen_width, screen_height = pyautogui.size()
+
+#initializing the relative flag to toggle relative mode
+relative = True
+
+#Variables to store previous position of the palm
+prev_x, prev_y = 0, 0
+hand_present = False
 
 # Tap detection variables
 tap_threshold = 0.02
@@ -35,6 +45,7 @@ while True:
     results = hands.process(frame_rgb)
     
     if results.multi_hand_landmarks:
+        hand_detected = False
         for hand_landmarks in results.multi_hand_landmarks:
             # Draw hand landmarks on the frame
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
@@ -47,12 +58,26 @@ while True:
             # Calculate the distance between thumb and index finger tips
             distance = ((index_finger_tip.x - thumb_tip.x) ** 2 + (index_finger_tip.y - thumb_tip.y) ** 2) ** 0.5
             
-            # Get screen coordinates
-            screen_x = int(palm.x * screen_width)
-            screen_y = int(palm.y * screen_height)
-            
-            # Move the mouse
-            pyautogui.moveTo(screen_x, screen_y)
+            #checking if relative moment or absolute
+            if relative:
+                #Get relative moment
+                if hand_present:
+                    delta_x = (palm.x - prev_x)*scaling_factor
+                    delta_y = (palm.y - prev_y)*scaling_factor
+
+                    #move the mouse by relative movement
+                    pyautogui.moveRel(delta_x*1000, delta_y*1000)
+                
+                prev_x, prev_y = palm.x, palm.y
+                hand_present = True
+                hand_detected = True
+            else:
+                # Get screen coordinates, multiplying scaling_factor for calibration of display
+                screen_x = int(palm.x * screen_width * scaling_factor)
+                screen_y = int(palm.y * screen_height * scaling_factor)
+                
+                # Move the mouse
+                pyautogui.moveTo(screen_x, screen_y)
             
             # Detect tap
             if distance < tap_threshold:
@@ -69,7 +94,12 @@ while True:
                 elif tap_count == 2:
                     pyautogui.doubleClick()
                     tap_count = 0  # Reset tap count after double click
-
+        if relative:
+            if not hand_detected:
+                hand_present = False  
+    else:
+        hand_present = False
+    
     # Display the frame
     cv2.imshow('Hand Tracking', frame)
     
